b3a36370fdb7c271f6ce828e89f413060c88bf50
pj-c0d3r <52074770+pj-c0d3r@users.noreply.github.com>
2020-11-04 17:14:13 +0100

    Added files via upload
    It begins

---
+++ index.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("jdAccordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("jdActive");
+        var jdPanel = this.nextElementSibling;
+        if (jdPanel.style.maxHeight) {
+            jdPanel.style.maxHeight = null;
+        } else {
+            jdPanel.style.maxHeight = jdPanel.scrollHeight + "px";
+        }
+    });
+}
